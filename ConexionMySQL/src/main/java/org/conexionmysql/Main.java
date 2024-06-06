7cd72deb9b888f2e81cd2d8aa5863769d08cdaef
Sebas <slopezo1@ucenfotec.ac.cr>
2024-06-06 11:38:56 -0600

    Se agrega el DAO (solamente con el agregar especialidad por el momento).
    Se crea el modelo de especialidades.
    
    Se configura el especialidades controller para que se agreguen los datos a la base de datos.
    
    Agrego en el main de manera comentada el código para agregar especialidades.

--- ConexionMySQL/src/main/java/org/conexionmysql/Main.java
+++ ConexionMySQL/src/main/java/org/conexionmysql/Main.java
@@ -2,6 +2,7 @@ package org.conexionmysql;
 
 import controller.ConexionController;
 import controller.CursoController;
+import controller.EspecialidadesController;
 import view.ConsoleView;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -19,6 +20,13 @@ public class Main {
         String nombre = "Matematicas";
         int estado = 1;
         cursoController.agregarCurso(nombre, estado);
+
+        //Especialidades agregar
+        /*ConsoleView consoleView = new ConsoleView();
+        EspecialidadesController especialidadesController = new EspecialidadesController(consoleView);
+        String nombre = "Neurología";
+        String descripcion = "La neurologia es la rama de la medicina que estudia el sistema nervioso. Específicamente se ocupa de la prevención, diagnóstico, tratamiento y rehabilitación de todas las enfermedades que involucran al sistema nervioso central, sistema nervioso periférico y el sistema nervioso autónomo.";
+        especialidadesController.agregarEspecialidad(nombre, descripcion);*/
 
 
     }
