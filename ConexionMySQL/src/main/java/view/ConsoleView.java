77767e2f028c1ee2c54c601904a8f8cf64ff24b0
Sebas <slopezo1@ucenfotec.ac.cr>
2024-06-10 14:47:14 -0600

    Implementación del mostrar por especialidad y mostrar todas las especialidades.

--- ConexionMySQL/src/main/java/view/ConsoleView.java
+++ ConexionMySQL/src/main/java/view/ConsoleView.java
@@ -1,5 +1,8 @@
 package view;
 
+import model.EspecialidadesModel;
+import java.util.List;
+
 public class ConsoleView {
 
     public void showMessage(String message){
@@ -8,6 +11,18 @@ public class ConsoleView {
 
     public void errorMessage(String message){
         System.err.println(message);
+    }
+
+    public void showEspecialidadDetails(EspecialidadesModel especialidad) {
+        System.out.println("Nombre: " + especialidad.getNombre());
+        System.out.println("Descripcion: " + especialidad.getDescripcion());;
+    }
+
+    public void showAllEspecialidades(List<EspecialidadesModel> especialidades) {
+        for (EspecialidadesModel especialidad : especialidades) {
+            showEspecialidadDetails(especialidad);
+            System.out.println("-------------------");
+        }
     }
 
 }
