433ef05858c211d2a06b086a64db519be6db4454
marcheloseII <asegurae@ucenfotec.ac.cr>
2024-06-11 11:06:34 -0600

    correcion

--- ConexionMySQL/src/main/java/org/conexionmysql/Main.java
+++ ConexionMySQL/src/main/java/org/conexionmysql/Main.java
@@ -12,7 +12,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello MySQL!");
-        ConsoleView viewConsole = new ConsoleView();
+        //ConsoleView viewConsole = new ConsoleView();
         //ConexionController conexionController = new ConexionController(viewConsole);
         //conexionController.openConnection();
         ConsoleView consoleView = new ConsoleView();
