9a56a615829a8c29986648107731f1713b471e78
marcheloseII <asegurae@ucenfotec.ac.cr>
2024-06-11 22:23:12 -0600

    ajuste en el main

--- ConexionMySQL/src/main/java/org/conexionmysql/Main.java
+++ ConexionMySQL/src/main/java/org/conexionmysql/Main.java
@@ -9,13 +9,8 @@ import java.sql.Date;
 public class Main {
     public static void main(String[] args) {
 
-            int opcion = scanner.nextInt();
-            scanner.nextLine();
-
-
         MenuView menuView = new MenuView();
         menuView.showMainMenu();
-
 
     }
 }
